9ff60d0b19113f967561b6c67e7cf034eeaef4d6
ananth-vaishnav <ananthavaishnav14@gmail.com>
2025-01-15 10:15:09 +0530

    first commit

---
+++ SRAapp/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class LoginModel(models.Model):
+    username = models.CharField(max_length=100, blank=True,null=True)
+    password = models.CharField(max_length=100, blank=True,null=True)
+    type = models.EmailField(max_length=100, blank=True,null=True)
+
+class User(models.Model):
+    LOGINID=models.ForeignKey(LoginModel,on_delete=models.CASCADE, blank=True,null=True)
+    name = models.CharField(max_length=100, blank=True,null=True)
+    email = models.CharField(max_length=100, blank=True,null=True)
+    profile_picture = models.FileField(upload_to='profile/', blank=True,null=True)
+    number=models.IntegerField(null=True,blank=True)
+    
